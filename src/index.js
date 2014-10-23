89a510e08af14e970a77f7e5525266474ff4c950
Chris Parker <mrcsparker@gmail.com>
2014-10-23 13:44:57 -0500

    Using traceur

--- src/index.js
+++ src/index.js
@@ -3,5 +3,7 @@
  * var Excellent = require('excellent').Excellent;
  */
 
+require('traceur/bin/traceur-runtime');
+
 exports.Excellent = require('./excellent.xlsx').Excellent;
 exports.ExcellentLoader = require('./excellent.loader').Excellent;
